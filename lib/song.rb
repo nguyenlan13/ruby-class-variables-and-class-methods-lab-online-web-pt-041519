700748634cf6947547e441839fd356fdfce598af
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-31 12:39:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,57 +1,114 @@
+# class Song
+  
+#   attr_accessor :name, :artist, :genre
+#   @@count = 0
+#   @@genres = []
+#   @@artists = []
+  
+#   def initialize (name, artist, genre)
+#     @name = name
+#     @artist = artist
+#     @genre = genre
+#     @@count +=1
+#     @@genres << genre
+#     @@artists << artist
+#   end
+  
+#   def self.count
+#     @@count
+#   end
+  
+#   def self.genre_count
+#     genre_count = {}
+#     @@genres.each do |genre|
+#       if genre_count[genre]
+#         genre_count[genre] += 1
+#       else
+#         genre_count[genre] = 1
+#       end
+#       end
+#         genre_count
+#     end
+  
+#   def self.genres
+#     @@genres.uniq
+#   end
+  
+#   def self.artists
+#     @@artists.uniq
+#   end
+  
+#   def self.artist_count
+#     artist_count = {}
+#     @@artists.each do |artist|
+#       if artist_count[artist]
+#         artist_count[artist] += 1
+#       else
+#         artist_count[artist] = 1
+#       end
+#       end
+#         artist_count
+#     end
+# end
+ 
+ 
+ 
+
 class Song
-  
-  attr_accessor :name, :artist, :genre
   @@count = 0
-  @@genres = []
   @@artists = []
+  @@genres = []
+
+attr_accessor :name, :artist, :genre
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count +=1
+  @@artists << artist
+  @@genres << genre
+end
+
+def self.count
+  @@count
+end
+
+def self.artists
+  @@artists.uniq
+end
+
+def self.genres
+  @@genres.uniq
+end
+
+def self.genre_count
   
-  def initialize (name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@count +=1
-    @@genres << genre
-    @@artists << artist
-  end
+end
+
+def self.artist_count
   
-  def self.count
-    @@count
-  end
-  
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-         genre_count[genre] += 1
-      else
-         genre_count[genre] = 1
-       end
-      end
-        genre_count
-    end
-  
-  def self.genres
-    @@genres.uniq
-  end
-  
-  def self.artists
-    @@artists.uniq
-  end
-  
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-         artist_count[artist] += 1
-      else
-         artist_count[artist] = 1
-       end
-      end
-        artist_count
-    end
- end
- 
- 
- 
+end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
